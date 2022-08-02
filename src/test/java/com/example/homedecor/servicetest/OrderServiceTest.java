8de3e6c91d7156769845c8595c8505f54a6ba46f
Rathore012 <lucky.rathore@globallogic.com>
2022-08-02 14:18:17 +0530

    orderTestCase

---
+++ src/test/java/com/example/homedecor/servicetest/OrderServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.homedecor.servicetest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.homedecor.dto.OrderByCustomer;
+import com.example.homedecor.exception.OrderException;
+import com.example.homedecor.service.OrderService;
+
+@SpringBootTest
+public class OrderServiceTest {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@AfterEach
+	void deleteOrderByIdTest() throws OrderException {
+		assertEquals(true, this.orderService.deleteOrderById(8));
+	}
+	
+	@Test
+	void addOrderTest() throws OrderException {
+		OrderByCustomer getOrder = new OrderByCustomer(8, "Pending", null);
+		assertTrue(orderService.addOrder(getOrder));
+		assertNotNull(orderService!=null);
+		
+	}
+	
+	@Test
+	void getOrderByIdTest() throws OrderException {
+		OrderByCustomer order = new OrderByCustomer(8, "Pending", null);
+		assertTrue(orderService.addOrder(order));
+		OrderByCustomer getOrder = orderService.getOrderById(8).get();
+		Integer orderId = getOrder.getOrderId();
+		assertEquals(orderId, 8);
+		assertNotNull(orderService.getOrderById(orderId));
+		
+	}
+
+}
