4d6ba132bd9fd38bbace1d972b49c958cbf72094
Rathore012 <lucky.rathore@globallogic.com>
2022-08-03 21:49:11 +0530

    OrderServiceTest with Coverage

--- src/test/java/com/homedecor/app/servicetest/OrderServiceTest.java
+++ src/test/java/com/homedecor/app/servicetest/OrderServiceTest.java
@@ -2,6 +2,7 @@ package com.homedecor.app.servicetest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.homedecor.app.dto.OrderByCustomer;
+import com.homedecor.app.exception.AdminException;
 import com.homedecor.app.exception.OrderException;
 import com.homedecor.app.service.OrderService;
 
@@ -19,17 +21,23 @@ import com.homedecor.app.service.OrderService;
 	@Autowired
 	private OrderService orderService;
 	
-	@AfterEach
+	@Test
 	void deleteOrderByIdTest() throws OrderException {
+		OrderByCustomer getOrder = new OrderByCustomer(8, "Pending", null);
+		assertTrue(orderService.addOrder(getOrder));
 		assertEquals(true, this.orderService.deleteOrderById(8));
+		assertThrows(OrderException.class,()->this.orderService.deleteOrderById(8));
 	}
 	
 	@Test
 	void addOrderTest() throws OrderException {
+		assertThrows(OrderException.class,()->this.orderService.addOrder(null));
 		OrderByCustomer getOrder = new OrderByCustomer(8, "Pending", null);
 		assertTrue(orderService.addOrder(getOrder));
 		assertNotNull(orderService);
-		
+		assertThrows(OrderException.class,()->this.orderService.addOrder(getOrder));
+		assertEquals(true, orderService.deleteOrderById(8));
+			
 	}
 	
 	@Test
@@ -40,6 +48,27 @@ import com.homedecor.app.service.OrderService;
 		Integer orderId = getOrder.getOrderId();
 		assertEquals(8,orderId);
 		assertNotNull(orderService.getOrderById(orderId));
+		assertEquals(true, orderService.deleteOrderById(8));
+		assertThrows(OrderException.class,()->this.orderService.getOrderById(8));
+		
+	}
+	
+	@Test
+	void getAllOrdersTest() throws OrderException {
+		OrderByCustomer order = new OrderByCustomer(8, "Pending", null);
+		assertTrue(orderService.addOrder(order));
+		assertNotNull(this.orderService.getAllOrders());
+		assertEquals(true, this.orderService.deleteOrderById(8));
+		assertThrows(OrderException.class,()-> this.orderService.getAllOrders());
+	}
+	
+	@Test
+	void updateOrderTest() throws OrderException {
+		OrderByCustomer order = new OrderByCustomer(8, "Pending", null);
+		assertTrue(orderService.addOrder(order));
+		assertNotNull(this.orderService.updateOrder(new OrderByCustomer(8, "Done", null)));
+		assertEquals(true, this.orderService.deleteOrderById(8));
+		assertThrows(OrderException.class,()-> this.orderService.updateOrder(new OrderByCustomer(8, "Successfull", null)));
 		
 	}
 
