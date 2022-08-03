bfaefe41c0450e1b5abc876de27ce50167c7b66a
Nikhil-2525043 <nikhil.narwat@globallogic.com>
2022-08-03 15:59:09 +0530

    update wishlist test cases and coverage

--- src/test/java/com/homedecor/app/servicetest/WishlistServiceTest.java
+++ src/test/java/com/homedecor/app/servicetest/WishlistServiceTest.java
@@ -3,12 +3,8 @@ package com.homedecor.app.servicetest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,26 +19,49 @@ import com.homedecor.app.service.WishlistService;
 	@Autowired
 	private WishlistService wishlistService;
 	
-	@AfterEach
+	@Test
 	void deleteWishlistById() throws WishlistException{
+		Wishlist wishlist = new Wishlist(1, null);
+		assertTrue(this.wishlistService.addWishlist(wishlist));
 		assertEquals(true, this.wishlistService.deleteWishlistById(1));
+		assertThrows(WishlistException.class, ()-> this.wishlistService.deleteWishlistById(1));
 	}
 	
 	@Test
 	void addWishlistTest() throws WishlistException{
+		assertThrows(WishlistException.class, ()-> this.wishlistService.addWishlist(null));
 		Wishlist wishlist = new Wishlist(1, null);
 		assertTrue(this.wishlistService.addWishlist(wishlist));
+		assertThrows(WishlistException.class, ()-> this.wishlistService.addWishlist(wishlist));
+		assertEquals(true, this.wishlistService.deleteWishlistById(1));
 	}
 	
 	@Test
 	void getWishlistByIdTest() throws WishlistException{
 		Wishlist wishlist = new Wishlist(1, null);
 		assertTrue(this.wishlistService.addWishlist(wishlist));
-//		assertNotNull(this.wishlistService.getWishlistById(1));
 		assertEquals(1, this.wishlistService.getWishlistById(1).get().getWishlistId());
-		assertEquals(1, this.wishlistService.getWishlistById(1).get().getWishlistId());
+		assertEquals(true, this.wishlistService.deleteWishlistById(1));
+		assertThrows(WishlistException.class,()-> this.wishlistService.getWishlistById(1));
 	}
 	
-
-
+	@Test
+	void getAllWishlistTest() throws WishlistException{
+		Wishlist wishlist = new Wishlist(1, null);
+		assertTrue(this.wishlistService.addWishlist(wishlist));
+		assertNotNull(this.wishlistService.getAllWishlists());
+		assertEquals(true, this.wishlistService.deleteWishlistById(1));
+		assertThrows(WishlistException.class,()-> this.wishlistService.getAllWishlists());
+	}
+	
+	@Test
+	void updateWishlistTest() throws WishlistException{
+		Wishlist wishlist = new Wishlist(1, null);
+		assertTrue(this.wishlistService.addWishlist(wishlist));
+		assertNotNull(this.wishlistService.updateWishlist(wishlist));
+		assertEquals(true, this.wishlistService.deleteWishlistById(1));
+		assertThrows(WishlistException.class,()->this.wishlistService.updateWishlist(wishlist));
+	}
+	
+	
 }
