110fd8ea6c36567bafe38a0b9c0f5e8275c691ef
Rathore012 <lucky.rathore@globallogic.com>
2022-08-16 12:53:54 +0530

    Admin  extra comments

--- src/main/java/com/homedecor/app/service/AdminServiceImpl.java
+++ src/main/java/com/homedecor/app/service/AdminServiceImpl.java
@@ -10,12 +10,32 @@ import com.homedecor.app.dao.AdminRepository;
 import com.homedecor.app.dto.Admin;
 import com.homedecor.app.exception.AdminException;
 
+/************************************************************************************
+ *          @author          Lucky Rathore
+ *          Description      It is a service class that provides the services for adding a new admin, 
+                             getting an admin by id, login into an existing admin account, updating admin password, delete admin by id.  
+  *         Version          1.0
+  *         Created Date     16-AUG-2022
+ ************************************************************************************/
+
+
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
 	@Autowired
 	private AdminRepository adminRepository;
+	
+	/************************************************************************************
+	 * Method: addAdmin
+     * Description: To register a new admin account in our app
+     * @Object admin                 - adding admin by providing mandatory admin details
+	 * @returns Boolean              - true, if admin added successfully otherwise throws AdminException
+	 * @throws AdminException      - It is raised if admin Id is already registered in the app
+     * Created By                    - Lucky Rathore
+     * Created Date                  - 16-AUG-2022                           
+	 
+	 ************************************************************************************/
 
 	@Override
 	public Boolean addAdmin(Admin admin) throws AdminException {
@@ -31,6 +51,19 @@ public class AdminServiceImpl implements AdminService {
 		return true;
 	}
 
+	/************************************************************************************
+	 * Method: getAdminById
+     * Description: To get an admin by AdminId
+     * 
+     * @param AdminId              - admin's Id
+	 * @returns Boolean            - isPresent, if admin is already present on the given Id in our database otherwise throws AdminException
+	 * @throws AdminException    - It is raised if admin Id is not present or we provide an invalid admin Id                                                   
+     * Created By                  - Lucky Rathore
+     * Created Date                - 16-AUG-2022                           
+	 
+	 ************************************************************************************/
+
+
 	@Override
 	public Optional<Admin> getAdminById(Integer adminId) throws AdminException {
 		Optional<Admin> foundAdminById = this.adminRepository.findById(adminId);
@@ -39,7 +72,21 @@ public class AdminServiceImpl implements AdminService {
 		}
 		return foundAdminById;
 	}
+	
+	/************************************************************************************
+	 * Method: Login
+     * Description: To login into an existing admin account
+     * 
+	 * @param adminId              - admin's Email 
+	 * @param password             - admin's password
+	 * @returns Boolean            - true, if Admin logged in successfully otherwise throws AdminException 
+	 * @throws AdminException    - It is raised due to incorrect Admin Id or password                                                      
+     * Created By                  - Lucky Rathore
+     * Created Date                - 16-AUG-2022                         
+	 
+	 ************************************************************************************/
 
+	
 	@Override
 	public Boolean login(Integer adminId, String password) throws AdminException {
 		Boolean isLoginBoolean = false;
@@ -51,6 +98,22 @@ public class AdminServiceImpl implements AdminService {
 		}
 		return isLoginBoolean;
 	}
+	
+	/************************************************************************************
+	 * Method: updatePassword
+     * Description: To update an admin account password
+     * 
+	 * @param adminId           - Admin's Email
+	 * @param oldPassword       - old adminPassword
+	 * @param newPassword       - new password to be updated
+	 * @returns Boolean         - true, if password for that adminId updated successfully otherwise throws AccountException
+	 * @throws AdminException   - It is raised if we provide invalid adminId or password                                                        
+     * Created By               - Lucky Rathore
+     * Created Date             - 16-AUG-2022                           
+	 
+	 ************************************************************************************/
+
+
 
 	@Override
 	public Boolean updatePassword(Integer adminId, String oldPassword, String newPassword) throws AdminException {
@@ -69,6 +132,20 @@ public class AdminServiceImpl implements AdminService {
 		}
 		return isLogin;
 	}
+	
+
+	/************************************************************************************
+	 * Method: deleteAdminById
+     * Description: To delete an existing admin by AdminId
+     * 
+     * @param AdminId              - admin's Id
+	 * @returns Boolean            - isdeleted, if admin is present in our database otherwise throws AdminException
+	 * @throws AdminException    -   It is raised if admin Id is not present or we provide an invalid admin Id                                                   
+     * Created By                  - Lucky Rathore
+     * Created Date                - 16-AUG-2022                           
+	 
+	 ************************************************************************************/
+	
 
 	@Override
 	public Boolean deleteAdminById(Integer adminId) throws AdminException {
